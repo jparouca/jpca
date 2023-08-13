088578beb0bc6145d023d006cd6e8fda4c7c0b3d
jparouca <joaopearoucaa@gmail.com>
2023-08-13 17:14:50 -0300

    no one reads, I know, and now I hope to never need to remember what I just commited

---
+++ app.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer(function(req, res) {
+  let filePath = '.' + req.url;
+  if (filePath === './') {
+    filePath = './index.html';
+  }
+
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    // Add other file types as needed
+  };
+
+  const contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, function(error, content) {
+    if (error) {
+      res.writeHead(500);
+      res.end('Sorry, an error occurred: ' + error.code + ' ..\n');
+      res.end();
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
+
